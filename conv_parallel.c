033ed6373c35f02e0dfb0276d1b2676dc1c85a3a
patthad <beebobbob956@gmai.com>
2024-10-30 01:27:36 +0700

    fix: 🐛 remove get clock time

--- conv_parallel.c
+++ conv_parallel.c
@@ -4,7 +4,6 @@
 
 int main() {
     // Start timing using OpenMP
-    double start_time = omp_get_wtime(); // Get the current wall-clock time
     // ---- input and malloc A, F ----
     int NA, NF;
     scanf("%d %d", &NA, &NF);
@@ -18,7 +17,7 @@ int main() {
         scanf("%d", &F[i]);
     }
     // ---- end input and malloc ----
-	int num_threads = (NA / 1000) < 1 ? 1 : (NA / 1000); // Example: 1 thread for every 1000 elements
+	int num_threads = (NF / 1000) < 1 ? 1 : (NF / 1000);
     if (num_threads > omp_get_max_threads()) {
         num_threads = omp_get_max_threads(); // Limit to max threads available
     }
@@ -40,11 +39,6 @@ int main() {
     for (int i = 0; i < NA - NF + 1; i++) {
         printf("%d\n", ans[i]);
     }
-
-    // End timing using OpenMP
-    double end_time = omp_get_wtime(); // Get the wall-clock time again
-    double time_taken = end_time - start_time; // Calculate the elapsed time
-    // printf("Time taken: %.6f seconds", time_taken); // Print the elapsed time
 
     // ---- free memory ----
     free(F);
